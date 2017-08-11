61dbda2f49aac31fc9c66977ecb780abf1328dfd
Fabian Gutierrez <fagossa@users.noreply.github.com>
2017-08-11 10:32:36 +0200

    Update 2_IntermediateTest.kt

--- src/test/kotlin/fr/xebia/xke/2_IntermediateTest.kt
+++ src/test/kotlin/fr/xebia/xke/2_IntermediateTest.kt
@@ -3,12 +3,14 @@ package fr.xebia.xke
 import io.kotlintest.matchers.shouldBe
 import io.kotlintest.specs.StringSpec
 
+
 class DataClassTest : StringSpec({
 
     val john = Director(
         name = "John Carpenter",
         bio = "was born in Carthage, New York")
 
+    // Task 2.1
     "it should provide a 'toString'" {
         john.toString() shouldBe "Director(name=John Carpenter, bio=was born in Carthage, New York, filmography=[])"
     }
@@ -29,6 +31,7 @@ class DataClassTest : StringSpec({
             mutableListOf("John Carpenter - Big Trouble in Little China")
     }
 
+    // Task 2.2 Companion objects
     "it should be able to use companion objects" {
         val csvReader = CsvFilmLoader.build("file.csv") { it shouldBe "Contents of FILE.CSV" }
         csvReader.actualPath shouldBe "FILE.CSV"
@@ -39,16 +42,17 @@ class DataClassTest : StringSpec({
 
 })
 
+// Task 2.5
 class GenericsTest : StringSpec({
 
-    "partitionWordsAndLines should" {
-        val (words, lines) = listOf("a", "a b", "c", "d e").partitionWordsAndLines()
+    "Lists should be able to split into words and lines" {
+        val (words, lines) = listOf("a", "a b", "c", "d e").splitWordsAndLines()
         listOf("a", "c") shouldBe words
         listOf("a b", "d e") shouldBe lines
     }
 
-    "testPartitionLettersAndOtherSymbols should" {
-        val (letters, other) = setOf('a', '%', 'r', '}').partitionLettersAndOtherSymbols()
+    "Lists should be able to split into letters and other symbols" {
+        val (letters, other) = setOf('a', '%', 'r', '}').splitLettersAndOthers()
         setOf('a', 'r') shouldBe letters
         setOf('%', '}') shouldBe other
     }
