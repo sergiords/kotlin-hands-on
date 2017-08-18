cfef6dc0a93c0a175fd852324df24dfa2e9bdf58
Fabian Gutierrez <fagossa@users.noreply.github.com>
2017-08-18 11:50:03 +0200

    [intermediate] add some test on collections

--- src/test/kotlin/fr/xebia/xke/2_IntermediateTest.kt
+++ src/test/kotlin/fr/xebia/xke/2_IntermediateTest.kt
@@ -1,26 +1,43 @@
 package fr.xebia.xke
 
+import fr.xebia.xke.MockFilmData.Companion.completeList
+import fr.xebia.xke.MockFilmData.Companion.hitchcockFilms
+import fr.xebia.xke.MockFilmData.Companion.kurosawaFilms
 import io.kotlintest.matchers.shouldBe
 import io.kotlintest.specs.StringSpec
 
+class MockFilmData {
+    companion object {
+        val ran = Film("Ran", 1985, Kurosawa, listOf(Action, Drama, War), 2.3)
+        val rashomon = Film("Rashomon", 1950, Kurosawa, listOf(Crime, Drama), 3.0)
+        val psyco = Film("Psycho", 1960, Hitchcock, listOf(Horror, Mistery, Thriller), 2.7)
+        val vertigo = Film("Vertigo", 1958, Hitchcock, listOf(Mistery, Romance, Thriller), 5.1)
+
+        val completeList = listOf<Film>(ran, rashomon, psyco, vertigo)
+        val hitchcockFilms = listOf<Film>(psyco, vertigo)
+        val kurosawaFilms = listOf<Film>(ran, rashomon)
+    }
+}
 
 class DataClassTest : StringSpec({
 
-    val john = Director(
+    val john = SpecialDirector(
         name = "John Carpenter",
         bio = "was born in Carthage, New York")
 
+    // ---------------------------------------
     // Task 2.1
-    "it should provide a 'toString'" {
-        john.toString() shouldBe "Director(name=John Carpenter, bio=was born in Carthage, New York, filmography=[])"
+    // ---------------------------------------
+    "TODO 2.1.1 - it should provide 'toString'" {
+        john.toString() shouldBe "SpecialDirector(name=John Carpenter, bio=was born in Carthage, New York, filmography=[])"
     }
 
-    "it should provide a 'copy'" {
+    "TODO 2.1.1 - it should provide 'copy'" {
         val updatedJohn = john.copy(name = "Mel Brooks")
         updatedJohn.bio shouldBe john.bio
     }
 
-    "it should be allow deconstructions " {
+    "TODO 2.1.2 - it should allow deconstructions " {
         val updatedJohn =
             john.copy(filmography =
             mutableListOf(
@@ -31,8 +48,10 @@ class DataClassTest : StringSpec({
             mutableListOf("John Carpenter - Big Trouble in Little China")
     }
 
+    // ---------------------------------------
     // Task 2.2 Companion objects
-    "it should be able to use companion objects" {
+    // ---------------------------------------
+    "TODO 2.2.3 - it should be able to use companion objects" {
         val csvReader = CsvFilmLoader.build("file.csv") { it shouldBe "Contents of FILE.CSV" }
         csvReader.actualPath shouldBe "FILE.CSV"
 
@@ -42,19 +61,41 @@ class DataClassTest : StringSpec({
 
 })
 
+// ---------------------------------------
 // Task 2.5
+// ---------------------------------------
 class GenericsTest : StringSpec({
 
-    "Lists should be able to split into words and lines" {
+    "TODO 2.5.1 - Lists should be able to split into words and lines" {
         val (words, lines) = listOf("a", "a b", "c", "d e").splitWordsAndLines()
         listOf("a", "c") shouldBe words
         listOf("a b", "d e") shouldBe lines
     }
 
-    "Lists should be able to split into letters and other symbols" {
+    "TODO 2.5.1 - Lists should be able to split into letters and other symbols" {
         val (letters, other) = setOf('a', '%', 'r', '}').splitLettersAndOthers()
         setOf('a', 'r') shouldBe letters
         setOf('%', '}') shouldBe other
     }
 
+})
+
+// ---------------------------------------
+// Task 2.7 Collections
+// ---------------------------------------
+class DataClassWithCollectionsTest : StringSpec({
+
+    "TODO 2.7.1 - it should use List#filter" {
+        getFilmsMadeBy(Hitchcock, completeList) shouldBe hitchcockFilms
+    }
+
+    "TODO 2.7.2 - it should use films List#filter and high order functions" {
+        filterFilmsUsingFilter(completeList, { it.director == Hitchcock }) shouldBe hitchcockFilms
+        filterFilmsUsingFilter(completeList, { it.director == Kurosawa }) shouldBe kurosawaFilms
+    }
+
+    "TODO 2.7.x - it should use folding to sum film prices" {
+        sumPricesWithFolding(hitchcockFilms) shouldBe 7.8
+        sumPricesWithFolding(kurosawaFilms) shouldBe 5.3
+    }
 })
