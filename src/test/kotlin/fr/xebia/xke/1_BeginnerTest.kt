ec097a75666dc1ea2bbe87a55ad8632196452e7f
Sergio Dos Santos <sergiords@users.noreply.github.com>
2017-08-30 23:41:02 +0200

    move lamdbas test to follow hands on order

--- src/test/kotlin/fr/xebia/xke/1_BeginnerTest.kt
+++ src/test/kotlin/fr/xebia/xke/1_BeginnerTest.kt
@@ -22,6 +22,35 @@ class PropertiesTest : StringSpec({
 
 })
 
+class FunctionsTest : StringSpec({
+
+    mapOf(
+        "http://example.com" to false,
+        "https://example.com" to true
+    ).forEach {
+        "${it.key} should be marked as ${if (it.value) "secure" else "insecure"}" {
+            isSecured(it.key) shouldBe it.value
+        }
+    }
+
+    mapOf(-1 to 1, 1 to 1, 2 to 2, 3 to 6, 4 to 24, 7 to 5040).forEach {
+        "${::factorial.name}(${it.key}) should be ${it.value}" {
+            factorial(it.key) shouldBe it.value
+        }
+    }
+
+    mapOf(
+        1 to "1 second(s)", 2 to "2 second(s)",
+        60 to "1 minute(s)", 120 to "2 minute(s)",
+        3600 to "1 hour(s)", 7200 to "2 hour(s)",
+        86400 to "1 day(s)", 172800 to "2 day(s)"
+    ).forEach {
+        "${::remainingTime.name}(${it.key}) should be ${it.value}" {
+            remainingTime(it.key) shouldBe it.value
+        }
+    }
+})
+
 class LambdasTest : StringSpec({
 
     mapOf(
@@ -56,35 +85,6 @@ class LambdasTest : StringSpec({
 
     }
 
-})
-
-class FunctionsTest : StringSpec({
-
-    mapOf(
-        "http://example.com" to false,
-        "https://example.com" to true
-    ).forEach {
-        "${it.key} should be marked as ${if (it.value) "secure" else "insecure"}" {
-            isSecured(it.key) shouldBe it.value
-        }
-    }
-
-    mapOf(-1 to 1, 1 to 1, 2 to 2, 3 to 6, 4 to 24, 7 to 5040).forEach {
-        "${::factorial.name}(${it.key}) should be ${it.value}" {
-            factorial(it.key) shouldBe it.value
-        }
-    }
-
-    mapOf(
-        1 to "1 second(s)", 2 to "2 second(s)",
-        60 to "1 minute(s)", 120 to "2 minute(s)",
-        3600 to "1 hour(s)", 7200 to "2 hour(s)",
-        86400 to "1 day(s)", 172800 to "2 day(s)"
-    ).forEach {
-        "${::remainingTime.name}(${it.key}) should be ${it.value}" {
-            remainingTime(it.key) shouldBe it.value
-        }
-    }
 })
 
 class TypeChecksSmartCastsTest : StringSpec({
