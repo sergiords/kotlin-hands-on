eb5f879da58c11fc9a8bb0cee03b5affc0b83c1d
Fabian Gutierrez <fagossa@gmail.com>
2018-04-16 21:13:46 +0200

    Add the exercise on FP without the solution
    See https://github.com/arrow-kt/arrow

---
+++ src/test/kotlin/fr/xebia/xke/5_FPTest.kt
@@ -0,0 +1,26 @@
+package fr.xebia.xke
+
+import arrow.data.NonEmptyList
+import arrow.data.invalid
+import arrow.data.valid
+import io.kotlintest.matchers.shouldBe
+import io.kotlintest.specs.StringSpec
+
+class FP_MockTest : StringSpec({
+
+    mapOf(
+        mapOf("name" to "Terah", "age" to "34") to User("Terah", 34).valid<NonEmptyList<String>, User>(),
+        mapOf("age" to "34") to NonEmptyList.of("name is not present").invalid(),
+        mapOf("name" to "", "age" to "34") to NonEmptyList.of("name is blank").invalid(),
+        mapOf("name" to "Terah") to NonEmptyList.of("age is not present").invalid(),
+        mapOf("name" to "Terah", "age" to "") to NonEmptyList.of("age is blank").invalid(),
+        mapOf("name" to "Terah", "age" to "???") to NonEmptyList.of("age must be an integer").invalid(),
+        mapOf("name" to "Terah", "age" to "-1") to NonEmptyList.of("-1 is negative").invalid(),
+        emptyMap<String, String>() to NonEmptyList.of("name is not present", "age is not present").invalid()
+    ).forEach {
+        "${it.key} should be ${it.value}" {
+            UserValidation().validateUser(it.key). shouldBe (it.value)
+        }
+    }
+
+})
